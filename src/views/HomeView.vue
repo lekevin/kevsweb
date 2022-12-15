f04650384a71ce39eb1bbe827785d337e6f0cdc0
lekevin <lekevvin@gmail.com>
2022-12-15 02:43:40 -0600

    added hero-section
    pretty much the first portion/div of the page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-
-// import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="encompass">
+    <div class="hero-section">
+      <h1 class="small-heading">Hi, I'm <span class="kevin">Kevin</span><span class="period" style="color: yellow">.</span></h1>
+      <h2 class="big-heading">I like to build ---------</h2>
+      <p class="description">I am a computer science major who is passionate about the functionality and design of websites and applications. Fueled by persistence and caffeine, I am currently seeking new opportunities within tech!</p>
+      <button class="courses">Check out my LinkedIn!</button>
+    </div>
+  </div>
+  <RouterView />
 </template>
