9e32f1bdd983a559c68f08deb4ba76f51fd49954
lekevin <lekevvin@gmail.com>
2023-01-09 09:41:07 +0700

    changed vite.config.ts
    vite was looking for path while compiling: changed config settings to ignore image absolutes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '../views/Homeview.vue';
+import HomeView from '../views/HomeView.vue';
 import Resume from '../views/Resume.vue';
 
 
