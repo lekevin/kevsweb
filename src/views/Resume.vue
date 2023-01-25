bdcb629d2bfef8fa0c007afdde431dc85f2c8732
lekevin <lekevvin@gmail.com>
2023-01-25 10:59:53 -0800

    Update Resume.vue

--- src/views/Resume.vue
+++ src/views/Resume.vue
@@ -3,7 +3,7 @@
 
 <template>
     <div id="app" style="height: 100vh;">
-      <embed src="resume.pdf" width="100%" height="100%" />
+      <embed src="resume2023.pdf" width="100%" height="100%" />
     </div>
   </template>
   
